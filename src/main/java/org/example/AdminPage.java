a7214a29f87ec884a65b724e12164dd2162fbf64
Andrey Sidorov <takandar@gmail.com>
2025-07-17 12:35:14 +0300

    добавила (ExpectedConditions.visibilityOfElementLocated для AdminPage

--- src/main/java/org/example/AdminPage.java
+++ src/main/java/org/example/AdminPage.java
@@ -6,7 +6,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.AssertJUnit;
 
 import java.time.Duration;
@@ -29,7 +31,7 @@ public class AdminPage {
        driver = SetUp.getDriver();
        driver.manage().window().maximize();
         driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(6));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(8));
         driver.findElement(By.name("username")).click();
         driver.findElement(By.name("username")).sendKeys("Admin");
         driver.findElement(By.xpath("//input[@type='password']")).click();
@@ -37,27 +39,31 @@ public class AdminPage {
         driver.findElement(By.tagName("button")).submit();
         String title = driver.getTitle();
         AssertJUnit.assertEquals("OrangeHRM", title);
-        // новый сценарий
+
         driver.findElement(By.className("oxd-main-menu-item")).click();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
        //выбрать user role
 
        driver.findElement(By.xpath("//div[contains(@class, 'oxd-select-text--after')]//i[contains(@class, 'oxd-icon')]")).click();
-       driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
-       driver.findElement(By.xpath("//div[@role='option' and contains(@class, 'oxd-select-option')][.//span[text()='Admin']]")).click();
+      // Ожидаем, пока пункт "Admin" станет видимым, и кликаем по нему
+       WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+       WebElement adminOption = wait.until(ExpectedConditions.visibilityOfElementLocated(
+               By.xpath("//div[@role='option' and contains(@class, 'oxd-select-option')]//span[text()='Admin']")));
+       adminOption.click();
+         
+       //  driver.findElement(By.xpath("//div[@role='option' and contains(@class, 'oxd-select-option')][.//span[text()='Admin']]")).click();
 
        // ввести username
        driver.findElement(By.xpath("//div[2]/input")).click();
        driver.findElement(By.xpath("//div[2]/input")).sendKeys("Admin");
-       driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+
        driver.findElement(By.xpath("//button[contains(.,'Search')]")).click();
 
 
         driver.findElement(By.xpath("//div[2]/i")).click();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+
         String userDropDown = driver.getCurrentUrl();
         AssertJUnit.assertEquals(userDropDown, "https://opensource-demo.orangehrmlive.com/web/index.php/admin/viewSystemUsers");
-
+        driver.quit();
     }
 
 }
