76e96f0be89898fb45c2390a01389e47c431a815
Anna <anna.atsulus@gmail.com>
2025-08-13 16:55:29 +0300

    c интерфейсом с методами с реализацией и без1

--- src/main/java/org/example/forinterface/Login.java
+++ src/main/java/org/example/forinterface/Login.java
@@ -1,22 +1,30 @@
 package org.example.forinterface;
 
+import org.example.ConfProperties;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public interface Login {
 
+
+
 WebDriver driver = new ChromeDriver();
+
+
 
     void submitbutton();
 
     
    default void enterusername() {
-        driver.findElement(By.name("username"));
+
+       String username = ConfProperties.getProperty("adminLogin");
+       driver.findElement(By.name("username")).sendKeys(username);
     }
 
     default void enterpassword() {
-        driver.findElement(By.name("password"));
+        String password = ConfProperties.getProperty("AdminPass");
+        driver.findElement(By.xpath("//input[@type='password']")).sendKeys(password);
     }
 
 }
--- src/main/java/org/example/forinterface/LoginUser.java
+++ src/main/java/org/example/forinterface/LoginUser.java
@@ -1,13 +1,42 @@
 package org.example.forinterface;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.example.SetUp;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.AssertJUnit;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class LoginUser implements Login{
 
     WebDriver driver = new ChromeDriver();
+
     @Override
     public void submitbutton() {
-      driver.findElement(By.name(    }
+        driver.findElement(By.name("submit")).click();
+
+    }
+
+
+    @Test
+    void LoginUserfromInterface() {
+       setup();
+       enterusername();
+        enterpassword();
+        submitbutton();
+        String title = driver.getTitle();
+        AssertJUnit.assertEquals("OrangeHRM", title);
+        String currentUrl = driver.getCurrentUrl();
+        AssertJUnit.assertEquals("https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index", currentUrl);
+        String adminName = driver.findElement(By.xpath("//h6[text()='Dashboard']")).getText();
+        AssertJUnit.assertEquals(adminName, "Dashboard");
+
+    }
+
+
 }
